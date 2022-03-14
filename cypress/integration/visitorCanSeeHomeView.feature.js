d5053346d708f673c750a2a2ff76efbc3713ed4c
giovanni <giacoletti92@gmail.com>
2022-03-14 23:31:27 +0100

    Add end2end test to check if visitor can navigate to user profile view clicking on relative link add next/link to home view to navigate to user profile view

--- cypress/integration/visitorCanSeeHomeView.feature.js
+++ cypress/integration/visitorCanSeeHomeView.feature.js
@@ -6,4 +6,14 @@ describe("A visitor navigating the main url", () => {
   it("is expected to see main header", () => {
     cy.get("[data-cy=main-header]").should("contain.text", "Welcome stranger!");
   });
+
+  describe("can click user profile link to navigate", () => {
+    before(() => {
+      cy.get("[data-cy=johnsmith21-link]").click();
+    });
+
+    it("is expected to navigate to user profile view", () => {
+      cy.url().should("include", "/johnsmith21");
+    });
+  });
 });
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1 data-cy="main-header">Welcome stranger!</h1>
+      <Link
+        prefetch={false}
+        href={{ pathname: "/[username]", query: { username: "johnsmith21" } }}
+      >
+        <a data-cy="johnsmith21-link">John's profile</a>
+      </Link>
     </div>
   );
 }
