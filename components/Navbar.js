c2468b556b6fda88063e518fa28a1dcbef7134cf
giovanni <giacoletti92@gmail.com>
2022-03-16 22:26:06 +0100

    Add end2end test to check if visitor can see the navbar with FEED and Log in buttons create navbar component with FEED and Log in buttons add navbar on top of all views

---
+++ components/Navbar.js
@@ -0,0 +1,45 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  const user = null,
+    username = null;
+
+  return (
+    <nav data-cy="navbar" className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button data-cy="feed-button" className="btn-logo">
+              FEED
+            </button>
+          </Link>
+        </li>
+
+        {username && (
+          <>
+            <li className="push-left">
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL} />
+              </Link>
+            </li>
+          </>
+        )}
+
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button data-cy="login-button" className="btn-blue">
+                Log in
+              </button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
--- cypress/integration/visitorCanSeeHomeView.feature.js
+++ cypress/integration/visitorCanSeeHomeView.feature.js
@@ -16,4 +16,18 @@ describe("A visitor navigating the main url", () => {
       cy.url().should("include", "/johnsmith21");
     });
   });
+
+  describe("can see navbar", () => {
+    it("is expected to see navbar component", () => {
+      cy.get("[data-cy=navbar]").should("be.visible");
+    });
+
+    it("is expected to have FEED button", () => {
+      cy.get("[data-cy=feed-button]").should("contain.text", "FEED");
+    });
+
+    it("is expected to have 'Log in' button", () => {
+      cy.get("[data-cy=login-button]").should("contain.text", "Log in");
+    });
+  });
 });
