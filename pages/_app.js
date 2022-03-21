7726149c0e79e55261e7fb5426fc9e6bb9dbffb5
giovanni <giacoletti92@gmail.com>
2022-03-21 19:56:29 +0100

    Refactor firebase auth and user data fetching into custom hook

--- pages/_app.js
+++ pages/_app.js
@@ -2,31 +2,13 @@ import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 import { Toaster } from "react-hot-toast";
 import { UserContext } from "../lib/context";
-import { auth, firestore } from '../lib/firebase';
-import { useEffect, useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
+import { useUserData } from "../lib/hooks";
 
 function MyApp({ Component, pageProps }) {
-  const [user] = useAuthState(auth);
-  const [username, setUsername] = useState(null);
-
-  useEffect(() => {
-    let unsubscribe;
-
-    if (user) {
-      const ref = firestore.collection("users").doc(user.uid);
-      unsubscribe = ref.onSnapshot((doc) => {
-        setUsername(doc.data()?.username);
-      });
-    } else {
-      setUsername(null);
-    }
-
-    return unsubscribe;
-  }, [user]);
+  const userData = useUserData();
 
   return (
-    <UserContext.Provider value={{ user, username }}>
+    <UserContext.Provider value={userData}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
