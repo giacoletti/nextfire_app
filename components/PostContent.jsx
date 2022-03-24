2f615d086e52af442a5d1cb3c44322297128da49
giovanni <giacoletti92@gmail.com>
2022-03-24 18:06:46 +0100

    Install react-markdown add ReactMarkdown component with post content to post full view add span element with data-cy attribute to post content

--- components/PostContent.jsx
+++ components/PostContent.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import ReactMarkdown from "react-markdown";
 
 // UI component for main post content
 export default function PostContent({ post }) {
@@ -11,13 +12,15 @@ export default function PostContent({ post }) {
     <div className="card">
       <h1 data-cy="post-header">{post?.title}</h1>
       <span data-cy="post-author-created" className="text-sm">
-        Written by{' '}
+        Written by{" "}
         <Link href={`/${post.username}`}>
           <a className="text-info">@{post.username}</a>
-        </Link>
-        {' '}on {createdAt.toISOString()}
+        </Link>{" "}
+        on {createdAt.toISOString()}
       </span>
-      <p data-cy="post-content">{post?.content}</p>
+      <span data-cy="post-content">
+        <ReactMarkdown>{post?.content}</ReactMarkdown>
+      </span>
     </div>
   );
 }
