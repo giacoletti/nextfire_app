6e6e1c88b403a87332257a770f858593146ef805
giovanni <giacoletti92@gmail.com>
2022-03-24 11:09:23 +0100

    Add end2end test to check if visitor can see full post view

--- cypress/integration/visitorCanSeeUserPostView.feature.js
+++ cypress/integration/visitorCanSeeUserPostView.feature.js
@@ -1,9 +1,25 @@
-describe("A visitor navigating to /[anything]/[alsoanything]", () => {
+describe("A visitor can see a full post by clicking on its title", () => {
   before(() => {
-    cy.visit("/wowowo/hereismypost");
+    cy.visit("/");
+    cy.get("[data-cy=post-title-0]").click();
   });
 
-  it("is expected to see 'User post' header", () => {
-    cy.get("[data-cy=post-header]").should("contain.text", "User post");
+  it("is expected to navigate to user post url", () => {
+    cy.url().should("contain", "/johnsmith92/first-post");
+  });
+
+  it("is expected to see post header", () => {
+    cy.get("[data-cy=post-header]").should("contain.text", "My first post!");
+  });
+
+  it("is expected to see post author and creation date", () => {
+    cy.get("[data-cy=post-author-created]").should(
+      "contain.text",
+      "Written by @johnsmith92 on 2022-03-22"
+    );
+  });
+
+  it("is expected to see post content", () => {
+    cy.get("[data-cy=post-content]").should("be.visible");
   });
 });
